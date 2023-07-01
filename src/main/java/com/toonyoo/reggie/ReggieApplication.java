4f87a6d95a69eb3089c569de710e0a34d8318de9
zhangzhen <zhangzhen@kiscloud.net>
2023-07-02 00:20:34 +0800

    初始化

--- src/main/java/com/toonyoo/reggie/ReggieApplication.java
+++ src/main/java/com/toonyoo/reggie/ReggieApplication.java
@@ -1,9 +1,11 @@
 package com.toonyoo.reggie;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.toonyoo.reggie.mapper")
 public class ReggieApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/toonyoo/reggie/ReggieApplicationTests.java
+++ src/test/java/com/toonyoo/reggie/ReggieApplicationTests.java
@@ -1,13 +1,23 @@
 package com.toonyoo.reggie;
 
+import com.toonyoo.reggie.mapper.UserMapper;
+import com.toonyoo.reggie.pojo.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ReggieApplicationTests {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
+        User user = userMapper.selectByPrimaryKey(1L);
+        log.info(String.valueOf(user));
     }
 
 }
